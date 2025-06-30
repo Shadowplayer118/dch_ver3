fbe4204f13a8df413dec42da6c0518d0768651f3
Lawrenz Xavier Carisusa <129159516+LawX22@users.noreply.github.com>
2025-06-30 11:43:19 +0800

    Improved login page but not finish yet

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRoutes from './AppRoutes'
+import './index.css';
+import './assets/css/Login.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
